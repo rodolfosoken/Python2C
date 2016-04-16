190980afbdf3d220773c5a8222de91ae2dfa3639
Rodolfo Soken <rodolfosoken@gmail.com>
2016-04-16 19:22:34 -0300

    primeiro commit

---
+++ src/lex/Tipo.java
@@ -0,0 +1,12 @@
+package lex;
+
+public class Tipo {
+	public final static int 
+	NUM = 256, 		//Numeros
+	ID=257, 		//identificadores
+	//Palavras reservadas
+	TRUE=258, 		
+	FALSE = 259,
+	IF = 260, 
+	WHILE = 261; //todo o resto da tabela vai aqui.
+}
---
+++ src/lex/Token.java
@@ -0,0 +1,11 @@
+package lex;
+
+public class Token {
+	final int tipo;
+	public Token(int tipo){
+		this.tipo = tipo;
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
