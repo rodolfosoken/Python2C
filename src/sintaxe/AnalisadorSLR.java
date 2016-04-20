dc4e05b89d972bd7de8c826b3d9192d4837cc8a9
Rodolfo Soken <rodolfosoken@gmail.com>
2016-04-20 00:32:02 -0300

    alteracao na ideia do indice da tabela

--- src/sintaxe/AnalisadorSLR.java
+++ src/sintaxe/AnalisadorSLR.java
@@ -13,14 +13,16 @@ public class AnalisadorSLR {
 		this.lex = lex;
 	}
 
-	private Estado action[][] = new Estado[100][100];
+	private Estado action[][] = new Estado[200][100];
 
 	void analisa() {
 		while (true) {
 			Stack<Estado> pilha = new Stack<>();
 			int top = pilha.pop().numero;
 			Token token = lex.analisa();
-			if (action[top][token.tipo].operacao == 's') {
+			int tipoToken = token.tipo;
+			if(tipoToken < 255) tipoToken-=40; //só para testes, mas a aquela ideia inicial estava certa...se ele achar um n-terminal que não seja um dos que nós declaramos, vai dar erro
+			if (action[top][tipoToken].operacao == 's') {
 
 			}
 
