6cdfe180d9ed4629ff018520dfdceae0441e049c
Rodolfo Soken <rodolfosoken@gmail.com>
2016-04-24 17:21:09 -0300

    Bug do leitor de tabelas resolvido
    começar os ajustes no lexico

--- src/sintaxe/lex/LeitorTabela.java
+++ src/sintaxe/lex/LeitorTabela.java
@@ -14,7 +14,7 @@ public class LeitorTabela {
 	private BufferedReader br;
 	private String url;
 	private String linha;
-	private int contLinha;
+	private int contLinha=0;
 
 	public LeitorTabela(String url) {
 		this.url = url;
@@ -27,16 +27,19 @@ public class LeitorTabela {
 
 	public void criaTabelaAction() throws IOException {
 		contLinha = 0;
-		while ((linha = br.readLine()) != null) {
+		while ((linha = br.readLine()) != null && contLinha < 163) {
 			String s[] = linha.split(";");
 			int i = 0;
 			for (String string : s) {
-				if (!string.isEmpty() && i < 51) { // delimitar para não pegar a
-													// goto
-					tabelaAction[contLinha][i++] = new Acao(string.trim().charAt(0),
-							Character.digit(string.trim().charAt(1), 10));
+				if ((!string.isEmpty()) && i < 51) { // delimitar para não pegar a goto
+					String[] res = string.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)"); //separa numeros e letras
+					if(res[0].charAt(0)=='a') 
+						tabelaAction[contLinha][i++] = new Acao('a', 0);
+					else tabelaAction[contLinha][i++] = new Acao(res[0].charAt(0),Integer.parseInt(res[1]));
 				} else if (!string.isEmpty()) {
-					tabelaGoto[contLinha][i - 51] = new Goto(String.valueOf(i - 51), Integer.parseInt(string.trim()));
+					tabelaGoto[contLinha][i - 51] = new Goto(String.valueOf(i-51),Integer.parseInt(string));
+					i++;
+				}else{
 					i++;
 				}
 
